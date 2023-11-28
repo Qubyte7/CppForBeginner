5e2b5452e17668c8b46d3de6cf7f719a42862b12
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-11-28 16:15:20 +0200

    quick sort bug solved

--- Sort/Quick.cpp
+++ Sort/Quick.cpp
@@ -1,55 +1,47 @@
 #include<iostream>
-#include<utility>
-#include<cstdlib>
 #include<ctime>
 using namespace std;
 
-int partition(int array[],int low,int high){
-    int pivot_index = low+(rand()%high-low+1);
+int partition(int array[], int low, int high) {
+    int pivot_index = low + (rand() % (high - low + 1));
 
-    if(pivot_index !=high){
-        swap(array[pivot_index],array[high]);
-    } 
-    int pivot_value= array[high];
+    if (pivot_index != high) {
+        swap(array[pivot_index], array[high]);
+    }
+    int pivot_value = array[high];
     int i = low;
-    for(int j=low;j<=high;j++){
-      if(array[j]<=pivot_value){
-        swap(array[i],array[j]);
-        i++;
-      }
+    for (int j = low; j <= high; j++) {
+        if (array[j] <= pivot_value) {
+            swap(array[i], array[j]);
+            i++;
+        }
     }
 
-    swap(array[i],array[high]);
+    swap(array[i], array[high]);
     return i;
-};
-
-void recursion_quickSort(int array[],int high,int low){
-    if(low>high){
-    int pivot = partition(array,low,high);
-    recursion_quickSort(array,low,pivot-1);
-    recursion_quickSort(array,pivot+1,high);
 }
-};
-void quickSort(int array[],int length){
-    srand(time(NULL));//is used to seed the random number generator provided by the rand function. The term "seed" refers to the initial value that is used to start the random number generation process.
-    //This ensures that each time the program is run, the seed is different, leading to a different sequence of random numbers.
-     recursion_quickSort(array,0,length-1);
-};
 
-
-int main(){
-    int array[]= {2,34,54,24,53,5,894,4,4};
-    int length  = sizeof(array)/sizeof(int);
-    quickSort(array,length);
-    for(int i=0;i<length;i++){
-        cout<< array[i]<< " ";
+void recursion_quickSort(int array[], int low, int high) {
+    if (low < high) {
+        int pivot = partition(array, low, high);
+        recursion_quickSort(array, low, pivot - 1);
+        recursion_quickSort(array, pivot + 1, high);
     }
-    cout<< endl;
-    cout<<"wewe";
+}
+
+void quickSort(int array[], int length) {
+    srand(time(NULL));
+    recursion_quickSort(array, 0, length - 1);
+}
+
+int main() {
+    int array[] = {2, 3, 9, 7, 3, 6, 7};
+    int length = sizeof(array) / sizeof(int);
+    quickSort(array, length);
+    for (int i = 0; i < length; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
 
     return 0;
-
 }
-
-
-
